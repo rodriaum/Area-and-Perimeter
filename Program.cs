76e52b5e1668e926274ab3869be40f3619091cc9
Rodrigão <rodrigofxxx04@gmail.com>
2023-11-02 19:55:36 +0000

    Adicionando Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AreaAndPerimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("CALCULADORA DE ÁREA E PERÍMETRO");
+
+            Console.WriteLine("Insira a largura do retângulo: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Insira a altura do retângulo: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double area = width * height;
+            double perimeter = 2 * (width + height);
+
+            Console.WriteLine("A área do retângulo é " + area + " e o peaametro é " + perimeter + ".");
+        }
+    }
+}
